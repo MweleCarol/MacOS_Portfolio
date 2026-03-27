4640d632634cb1e63959e4347756ab947f7e32cb
MweleCarol <mwelecarolyne65@gmail.com>
2026-03-27 11:21:26 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from '#Components/Navbar';
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
