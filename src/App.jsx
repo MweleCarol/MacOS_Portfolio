e45135ac046db8edca6ce3303bd5111e6f4d2013
MweleCarol <mwelecarolyne65@gmail.com>
2026-04-01 11:21:30 +0300

    Implemented the first part of the finder window

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import gsap from 'gsap';
 import Terminal from '#windows/Terminal';
 import Safari from '#windows/Safari';
 import Resume from '#windows/Resume';
+import Finder from '#windows/Finder';
 
 gsap.registerPlugin(Draggable);
 
@@ -19,6 +20,7 @@ const App = () => {
       <Terminal/>
       <Safari/>
       <Resume/>
+      <Finder/>
     </div>
   );
 }
